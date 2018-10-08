6eb67189677c87dc6d3d480da2e5a33366538973
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-08 20:37:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ function destructivelyRemoveFirstKitten() {
   kittens.shift();
 }
 
-function appendKitten(name) {
-  
-}
-
 function removeLastKitten() {
   return kittens.slice(0, kittens.length-1);
 }
 
 function removeFirstKitten() {
   return kittens.slice(1, kittens.length);
+}
+
+function appendKitten(name) {
+  
 }
